bfc32572b3c9151c46d5e281b264deec9049f48e
ramzan_abdulnur <gordeew.r2015@yandex.ru>
2025-04-28 19:06:03 +0300

    vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',  // <-- Прямо указать путь, без использования path.resolve
     },
-  }
+  },
 })
